be4ee69ed14b5ba38c040932dafda1f356c9dab6
hejiang <18715770814@163.com>
2020-12-03 17:48:31 +0800

    1. 打通sample引用redis项目

--- src/main/java/com/ql/qlredis/autoconfigure/annotations/EnableQlRedis.java
+++ src/main/java/com/ql/qlredis/autoconfigure/annotations/EnableQlRedis.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Import;
 import java.lang.annotation.*;
 
 /**
+ * EnableQlRedis：sample使用这个注解，就能执行redis的QlRedisConfiguration中的自动扫描（ComponentScan）
+ *
  * Documented功能：指明修饰的注解，可以被例如javadoc此类的工具文档化，只负责标记，没有成员取值。
  */
 @Retention(value = RetentionPolicy.RUNTIME)
