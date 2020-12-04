05226f8a8bfe6e387bd8eab7a53adf4ae46cbcd8
hejiang <18715770814@163.com>
2020-12-04 16:40:40 +0800

    1. 添加注释注解

--- src/main/java/com/ql/qlredis/autoconfigure/annotations/EnableQlRedis.java
+++ src/main/java/com/ql/qlredis/autoconfigure/annotations/EnableQlRedis.java
@@ -9,6 +9,7 @@ import java.lang.annotation.*;
  * EnableQlRedis：sample使用这个注解，就能执行redis的QlRedisConfiguration中的自动扫描（ComponentScan）
  *
  * Documented功能：指明修饰的注解，可以被例如javadoc此类的工具文档化，只负责标记，没有成员取值。
+ * @author jiang.he
  */
 @Retention(value = RetentionPolicy.RUNTIME)
 @Target(value = ElementType.TYPE)
